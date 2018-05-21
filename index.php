81d3d81fd2875362450492f1a5123c3e14345ce4
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2018-05-21 13:50:16 +0300

    Customize

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Agrevia</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- styles -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700" rel="stylesheet">
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="assets/css/docs.css" rel="stylesheet">
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet">
+  <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+  <link href="assets/css/flexslider.css" rel="stylesheet">
+  <link href="assets/css/sequence.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/color/default.css" rel="stylesheet">
+
+  <!-- fav and touch icons -->
+  <link rel="shortcut icon" href="assets/ico/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+
+  <style>
+
+  
+  section#intro{
+	  background: 0;
+	  border-color: rgba(0,0,0,0);
+  }
+  
+  video { 
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+    transform: translateX(-50%) translateY(-50%);
+ background: url('//demosthenes.info/assets/images/polina.jpg') no-repeat;
+  background-size: cover;
+  transition: 1s opacity;
+  opacity: 0.2;
+}
+
+p{
+	font-size:14px ;
+	color: rgb(10,10,10);
+}
+
+  </style>
+</head>
+
+<body>
+  <header>
+ 
+  </header>
+  <section id="intro">
+  	<video poster="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC" id="bgvid" playsinline autoplay muted loop>
+  <!-- 
+- Video needs to be muted, since Chrome 66+ will not autoplay video with sound.
+WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+
+<source src="assets/img/slides/sequence/bg.mp4" type="video/mp4">
+
+</video>
+    <div class="jumbotron masthead">
+      <div class="container">
+        <!-- slider navigation -->
+        <div class="sequence-nav">
+          <div class="prev">
+            <span></span>
+          </div>
+          <div class="next">
+            <span></span>
+          </div>
+        </div>
+        <!-- end slider navigation -->
+        <div class="row">
+          <div class="span12">
+            <div id="slider_holder">
+              <div id="sequence">
+
+	
+                <ul>
+                  <!-- Layer 1 -->
+                  <li>
+                    <div class="info animate-in">
+                      <h2>ΔΗΛΩΣΕΙΣ ΟΣΔΕ</h2>
+                      <p>
+                        Ενιαία Αίτηση Ενίσχυσης, Πράσινη Ενίσχυση, Συνδεδεμένες Ενισχύσεις, Μεταβιβάσεις Δικαιωμάτων
+                      </p>
+                      <a class="btn btn-success" href="#">Μάθετε περισσότερα &raquo;</a>
+                    </div>
+                    <img class="slider_img animate-in" src="assets/img/logo-dark.png" alt="">
+					
+                  </li>
+                  <!-- Layer 2 -->
+                  <li>
+                    <div class="info">
+                      <h2>ΑΓΡΟΤΕΣ</h2>
+                      <p>
+                        Νέοι Αγρότες, Σχέδια Βελτίωσης, Νιτρορύπανση, Βιολογική Γεωργία, Μελέτες, Επιδοτήσεις, Επιστροφές, Άδειες
+                      </p>
+                      <a class="btn btn-success" href="#">Μάθετε περισσότερα &raquo;</a>
+                    </div>
+					<!--
+                    <img class="slider_img" src="assets/img/slides/sequence/img-2.png" alt="">
+					-->
+                  </li>
+                  <!-- Layer 3 -->
+                  <li>
+                    <div class="info">
+                      <h2>ΚΤΗΝΟΤΡΟΦΟΙ</h2>
+                      <p>
+                        Νέοι Κτηνοτρόφοι, Βιολογική Κτηνοτροφία, Τήρηση Μητρώων, Σπάνιες Φυλές, Μελέτες, Επιδοτήσεις, Επιστροφές
+                      </p>
+                      <a class="btn btn-success" href="#">Μάθετε περισσότερα &raquo;</a>
+                    </div>
+					<!--
+                    <img class="slider_img" src="assets/img/slides/sequence/img-3.png" alt="">
+					-->
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Sequence Slider::END-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="maincontent">
+    <div class="container">
+	<h2>Σύμμαχος σας στην ανάπτυξη!</h2>
+	<br>
+	<p>Το γραφείο μας από τον Μάρτιο του 2014 έχει πιστοποιηθεί ως φορέας Α του ΟΠΕΚΕΠΕ (ΤΑΝΤΟΣ ΔΗΜΗΤΡΗΣ με αριθμό απόφασης 274/38602 - 27/3/2014) για την υποβοήθηση των αγροτών του Νομού Καρδίτσας για τη συμπλήρωση και υποβολή της Ενιαίας Αίτησης Ενίσχυσης στο ΟΣΔΕ έγκυρα και έγκαιρα. Επίσης, το ΑΓΡΟΛΥΣΙΣ έχει πιστοποιηθεί από το ΥΠΑΠΕΝ ως Κέντρο Εξετάσεων χορήγησης πιστοποιητικού γνώσεων ορθολογικής χρήσης των γεωργικών φαρμάκων.</p>
+     <br>
+	 <br>
+	 <br>
+	 <br>
+	 <div class="row">
+        <div class="span3 features">
+          <i class="icon-circled icon-32 icon-suitcase left"></i>
+          <h4>Αγροτική Ανάπτυξη</h4>
+          <div class="dotted_line">
+          </div>
+          <p class="left">
+            Έχοντας αναπτύξει εξειδικευμένες υπηρεσίες άμεσης και υπεύθυνης στήριξης και ενημέρωσης για κάθε είδους επιδοτούμενου προγράμματος που αφορά τον αγροδιατροφικό τομέα,η εταιρεία μας αποτελεί την πρώτη επιλογή σας στην παροχή ολοκληρωμένων συμβουλευτικών υπηρεσιών.
+          </p>
+          
+        </div>
+        <div class="span3 features">
+          <i class="icon-circled icon-32 icon-plane left"></i>
+          <h4>Επιχειρηματικότητα</h4>
+          <div class="dotted_line">
+          </div>
+          <p class="left">
+            Η εταιρεία μας είναι σε θέση να σας παρέχει ενημέρωση και συμβουλευτική υποστήριξη για όλα τα επιδοτούμενα προγράμματα που μπορεί να σας αφορούν,στηρίζοντας τις αναπτυξιακές σας κινήσεις.Επίσης αναλαμβάνει την εκπόνηση της μελέτης και την επιστημονική σχεδίαση του Διαγράμματος Ροής της δικής σας επιχείρησης με σκοπό τη διευκόλυνση και την έγκαιρη αδειοδότηση της.
+          </p>
+          
+        </div>
+        <div class="span3 features">
+          <i class="icon-circled icon-32 icon-leaf left"></i>
+          <h4>Εκπαίδευση</h4>
+          <div class="dotted_line">
+          </div>
+          <p class="left">
+            Η τάση επιστροφής στη φύση επιδέχεται πολλές αναγνώσεις.Η εταιρεία μας με μια σύγχρονη ματιά στο μέλλον,καινοτομεί,με βασική επιδίωξή μας την παροχή έγκυρης ενημέρωσης,εξειδίκευσης και κατάρτισης όσον αφορά τα προϊόντα και τις υπηρεσίες του αγροτικού τομέα.
+          </p>
+         
+        </div>
+        <div class="span3 features">
+          <i class="icon-circled icon-32 icon-wrench left"></i>
+          <h4>Περιβάλλον</h4>
+          <div class="dotted_line">
+          </div>
+          <p class="left">
+            Στηριζόμενοι στο τρίπτυχο «Συνέπεια – Εργατικότητα – Σεβασμός στο περιβάλλον»,παρέχουμε ολοκληρωμένες λύσεις σε ζητήματα διαχείρισης περιβάλλοντος και φυσικών πόρων,με στόχο να δώσουμε εναλλακτικές μεθόδους σε επιχειρήσεις και αγρότες,ώστε να γίνουν βιώσιμες οικολογικά και οικονομικά οι δραστηριότητες τους.
+          </p>
+          
+        </div>
+      </div>
+      <div class="row">
+        <div class="span12">
+          <div class="tagline centered">
+            <div class="row">
+              <div class="span12">
+                <div class="tagline_text">
+                  <h2>ΑΝΑΚΟΙΝΩΣΗ? </h2>
+                  <p>
+                    Εντός εβδομάδας ανοίγουν νέα προγράμματα του ΕΣΠΑ
+                  </p>
+                </div>
+                <div class="btn-toolbar cta">
+                  <a class="btn btn-large btn-color" href="#">
+							<i class="icon-white"></i> Μάθετε περισσότερα </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end tagline -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="home-posts">
+          <div class="span12">
+            <h3>Τελευταία Νέα</h3>
+          </div>
+          <div class="span3">
+
+            <div class="entry-meta">
+
+              <span class="date">Sep 17 <br>
+					2013</span>
+            </div>
+            <!-- end .entry-meta -->
+            <div class="entry-body">
+              <a href="post_right_sidebar.html">
+                <h5 class="title">This is a standard post</h5>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet nec, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa.
+              </p>
+            </div>
+            <!-- end .entry-body -->
+            <div class="clear">
+            </div>
+          </div>
+          <div class="span3">
+            <div class="entry-meta">
+ 
+              <span class="date">Sep 17 2013</span>
+            </div>
+            <!-- end .entry-meta -->
+            <div class="entry-body">
+              <a href="post_right_sidebar.html">
+                <h5 class="title">Example post image format</h5>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet nec, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa.
+              </p>
+            </div>
+            <!-- end .entry-body -->
+            <div class="clear">
+            </div>
+          </div>
+          <div class="span3">
+            <div class="entry-meta">
+              <span class="date">Sep 17 2011</span>
+            </div>
+            <!-- end .entry-meta -->
+            <div class="entry-body">
+              <a href="post_right_sidebar.html">
+                <h5 class="title">Example post image format</h5>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet nec, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque.
+              </p>
+            </div>
+            <!-- end .entry-body -->
+            <div class="clear">
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer
+ ================================================== -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="span8">
+          <div class="widget">
+            <h5>Σχετικά</h5>
+Η εταιρεία "ΑΓΡΟΛΥΣΙΣ" ιδρύθηκε τον Φεβρουάριο του 2014 από τους γεωπόνους μελετητές Τάντο Κ. Δημήτριο, Μανόπουλο Ε. Λάμπρο και Κουτσώνη Γ. Ευάγγελο.
+
+Σκοπός των ανθρώπων της ΑΓΡΟΛΥΣΙΣ είναι η λειτουργία μιας σύγχρονης εταιρείας που να παρέχει ένα ολοκληρωμένο φάσμα υπηρεσιών τόσο στον αγρο-κτηνοτροφικό πληθυσμό όσο και στις επιχειρήσεις του νομού Καρδίτσας.
+
+Οι δραστηριότητες του γεωτεχνικού γραφείου ΑΓΡΟΛΥΣΙΣ και του πιστοποιημένου φορέα ΤΑΝΤΟΣ Δ. καλύπτουν το σύνολο των υπηρεσιών που μπορούν να προσφερθούν στον αγροπεριβαλλοντικό χώρο με την παροχή τεχνικής βοήθειας, συμβουλευτικών υπηρεσιών, την σύνταξη και υλοποίηση επενδυτικών σχεδίων, την κατάρτιση των ατόμων που ασχολούνται με τον πρωτογενή τομέα και τους τομείς της πρώτης μεταποίησης.
+          </div>
+        </div>
+
+        <div class="span4">
+          <div class="widget">
+            <!-- logo -->
+            <a class="brand logo" href="index.html">
+							<img src="assets/img/logo-dark.png" alt="" />
+						</a>
+            <!-- end logo -->
+            <address>
+							<strong>Αγροσυμβουλευτική Ευβοίας</strong><br>
+							 Ληλαντίων 58, Χαλκίδα<br>
+							 34100, Εύβοια<br>
+							<abbr title="Phone">P:</abbr> (+30) 2221309971<br>
+							Email: <a href="mailto: info@agrosymbouleutiki.gr">info@agrosymbouleutiki.gr</a><br>
+						</address>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span6">
+            <p>
+              &copy; Agrevia - All right reserved
+            </p>
+          </div>
+          <div class="span6">
+            <div class="credits">
+              Designed by <a href="mailto: tsiktsiris@sch.gr">Dimitris Tsiktsiris</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- JavaScript Library Files -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.easing.js"></script>
+  <script src="assets/js/google-code-prettify/prettify.js"></script>
+  <script src="assets/js/modernizr.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/jquery.elastislide.js"></script>
+  <script src="assets/js/sequence/sequence.jquery-min.js"></script>
+  <script src="assets/js/sequence/setting.js"></script>
+  <script src="assets/js/jquery.prettyPhoto.js"></script>
+  <script src="assets/js/application.js"></script>
+  <script src="assets/js/jquery.flexslider.js"></script>
+  <script src="assets/js/hover/jquery-hover-effect.js"></script>
+  <script src="assets/js/hover/setting.js"></script>
+
+  <!-- Template Custom JavaScript File -->
+  <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
